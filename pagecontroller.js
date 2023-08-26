be2e88606b3bb39e49afd6e53abc5fa9e8d60855
vishalmgd <124760695+vishalmgd@users.noreply.github.com>
2023-08-26 23:14:08 +0530

    Update pagecontroller.js

--- pagecontroller.js
+++ pagecontroller.js
@@ -1,106 +1,71 @@
-
 const axios = require("axios");
-
+const path = require("path"); // Add this line to import the path module
+const fs = require("fs");
 const puppeteer = require("puppeteer");
 
 const showhomepage = async (req, res) => {
+  res.render("index", {
+    downloadUrl: "",
+  });
+};
 
-  
-      res.render("index");
-    // }
-  };
-
-
-  
 const twitterpost = async (req, res) => {
-    const url = req.body.url;
-    console.log("u r here top================================================");
-    console.log(url);
-  
-    if (!url) {
-      const error = ErrorHandler.validationError("Please provide a valid URL!");
-      return res.status(error.status).json({ message: error.message });
-    }
-    const tweetUrl = url;
-    // console.log("Executable Path:", puppeteer.executablePath());
-    try {
-      const getVideoUrl = async (tweetUrl) => {
-        // const browser = await puppeteer.launch();
-        const browser = await puppeteer.launch({
-          headless: true,
-          args: [
-            "--disable-setuid-sandbox",
-            "--no-sandbox",
-            "--single-process",
-            "--no-zygote",
-          ],
-          executablePath:
-            process.env.NODE_ENV === "production"
-              ? process.env.PUPPETEER_EXECUTABLE_PATH
-              : puppeteer.executablePath(),
+  const url = req.body.url;
+
+  if (!url) {
+    return res.status(400).json({ error: "Please provide a valid URL!" });
+  }
+
+  try {
+    const getVideoUrl = async (tweetUrl) => {
+      const browser = await puppeteer.launch({
+        headless: true,
+        args: [
+          "--disable-setuid-sandbox",
+          "--no-sandbox",
+          "--single-process",
+          "--no-zygote",
+        ],
+        executablePath:
+          process.env.NODE_ENV === "production"
+            ? process.env.PUPPETEER_EXECUTABLE_PATH
+            : puppeteer.executablePath(),
+      });
+
+      const page = await browser.newPage();
+
+      try {
+        await page.goto(tweetUrl, { timeout: 90000 });
+        await page.waitForSelector("video", { timeout: 90000 });
+
+        const tweetData = await page.evaluate(() => {
+          const videoElement = document.querySelector("video");
+          return {
+            videoUrl: videoElement.src,
+          };
         });
-  
-        const page = await browser.newPage();
-        // await page.setUserAgent('Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.182 Safari/537.36');
-  
-        try {
-          // await page.goto(tweetUrl);
-          await page.goto(tweetUrl, { timeout: 90000 }); // Timeout set to 10 seconds (10,000 milliseconds)
-  
-          // await page.waitForSelector("video");
-          await page.waitForSelector("video", { timeout: 90000 }); // Timeout set to 5 seconds (5,000 milliseconds)
-  
-          const tweetData = await page.evaluate(
-            () => {
-              const videoElement = document.querySelector("video");
-    
-              return {
-                videoUrl: videoElement.src,
-              };
-            },
-            { timeout: 90000 } // Timeout set to 90 seconds (90,000 milliseconds)
-          );
-          console.log(tweetData);
-  
-          return tweetData.videoUrl;
-        } catch (error) {
-          console.error("Error:", error);
-        } finally {
-          await browser.close();
-        }
-      };
-  
-      const videoUrl = await getVideoUrl(tweetUrl);
-  
-  
-    // res.render("index", {
-    //     title: "Twitterpage",
 
-    //   });
-  
-      ////////
-  
-      if (videoUrl) {
-        // Instead of downloading the video, send the video URL to the client
-        //  console.log("outside getVideoUrl function",videoUrl);
-        // handleFormSubmit(videoUrl, res);
-        res.json({ videoUrl: videoUrl });
-        // res.render("twitterpage", {
-        //   title: "Twitterpage",
-       
-        // });
-      } else {
-        res.status(404).json({ error: "Video URL not found." });
+        return tweetData.videoUrl;
+      } catch (error) {
+        console.error("Error:", error);
+      } finally {
+        await browser.close();
       }
-    } catch (err) {
-      console.log(err);
-  
-      const error = ErrorHandler.validationError(
-        "Invalid URL or Twitter/User imposed some restriction"
-      );
-      return res.status(error.status).json({ message: error.message });
-    }
-  };
-  
+    };
 
- module.exports = { showhomepage,twitterpost  };
+    const videoUrl = await getVideoUrl(url);
+    const response = await axios.get(videoUrl, { responseType: "arraybuffer" });
+    console.log(response.data);
+    if (videoUrl) {
+      // Instead of saving the file, send the video URL to the client
+      res.render("index", { downloadUrl: videoUrl });
+  } else {
+      res.status(404).json({ error: "Video URL not found." });
+  }
+  } catch (err) {
+    console.log(err);
+    return res.status(500).json({ message: "An error occurred." });
+  }
+};
+
+module.exports = { showhomepage, twitterpost };
