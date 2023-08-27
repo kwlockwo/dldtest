6aff8827137c3c06ee0ee539be42bc06e6170e4d
vishalmgd <124760695+vishalmgd@users.noreply.github.com>
2023-08-27 13:16:06 +0530

    little change pagecontroller.js

--- pagecontroller.js
+++ pagecontroller.js
@@ -1,6 +1,5 @@
 const axios = require("axios");
-const path = require("path"); // Add this line to import the path module
-const fs = require("fs");
+
 const puppeteer = require("puppeteer");
 
 const showhomepage = async (req, res) => {
@@ -34,19 +33,19 @@ const twitterpost = async (req, res) => {
 
       const page = await browser.newPage();
 
-      try {
-        await page.goto(tweetUrl, { timeout: 90000 });
-        await page.waitForSelector("video", { timeout: 90000 });
-
-        const tweetData = await page.evaluate(() => {
-          const videoElement = document.querySelector("video");
-          return {
-            videoUrl: videoElement.src,
-          };
-        });
-        console.log("==================================")
-        console.log("tweetdata url",tweetData.videoUrl)
-        return tweetData.videoUrl;
+      try {        
+        await page.goto(tweetUrl, { waitUntil: "domcontentloaded", timeout: 30000 });
+        const videoElementHandle = await page.waitForSelector("video", { timeout: 10000 });
+        if (!videoElementHandle) {
+          throw new Error("Video element not found");
+        }
+        const videoUrl = await page.evaluate((videoElement) => {
+          return videoElement.src;
+        }, videoElementHandle);
+    
+        return videoUrl;
+    
+ 
       } catch (error) {
         console.error("Error:", error);
       } finally {
@@ -55,19 +54,16 @@ const twitterpost = async (req, res) => {
     };
 
     const videoUrl = await getVideoUrl(url);
-    const response = await axios.get(videoUrl, { responseType: "arraybuffer" });
-    console.log("response.data ++++++++++++++++++++++++++++++++++++++++++++++++");
-    console.log(response.data);
-    console.log("response.data ++++++++++++++++++++++++++++++++++++++++++++++++");
+
     if (videoUrl) {
       // Instead of saving the file, send the video URL to the client
+      console.log("Outside fn",videoUrl);
       res.render("index", { downloadUrl: videoUrl });
   } else {
       res.status(404).json({ error: "Video URL not found." });
   }
   } catch (err) {
     console.log(err);
-    // return res.status(500).json({ message: "An error occurred." });
     return res.status(500).json({ message: err });
   }
 };
